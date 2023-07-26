370ea535e56602e3e5cf7c35061db9d396da686a
Noah Too <krivahtoo@gmail.com>
2023-07-26 08:52:51 +0300

    add 0x1B task 0. Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * swap - swap two values
+ *
+ * @a: pointer to integer value
+ * @b: pointer to integer value
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order using the
+ * Bubble sort algorithm
+ *
+ * @array: array of integers to sort
+ * @size: the size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < (size - 1); i++)
+	{
+		for (j = 0; j < (size - i - 1); j++)
+		{
+			if (*(array + j) > *(array + j + 1))
+			{
+				swap(array + j, array + j + 1);
+				print_array(array, size);
+			}
+		}
+	}
+}
