2087d5f56785d7c1202fa3f4195c1f5cb324d35a
Noah Too <krivahtoo@gmail.com>
2023-07-30 18:16:15 +0300

    fix: betty errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -27,17 +27,16 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	if (array == NULL)
-		return
-	for (i = 0; i < (size - 1); i++)
-	{
-		for (j = 0; j < (size - i - 1); j++)
+	if (array != NULL)
+		for (i = 0; i < (size - 1); i++)
 		{
-			if (*(array + j) > *(array + j + 1))
+			for (j = 0; j < (size - i - 1); j++)
 			{
-				swap(array + j, array + j + 1);
-				print_array(array, size);
+				if (*(array + j) > *(array + j + 1))
+				{
+					swap(array + j, array + j + 1);
+					print_array(array, size);
+				}
 			}
 		}
-	}
 }
